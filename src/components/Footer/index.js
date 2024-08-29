d5de91e96960e3a52286986555d31ebd642cd73e
willsouzaa <pauloamancio79@gmail.com>
2024-08-29 11:06:31 -0300

    Projeto inicial v1

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import style from "./Footer.module.css";
 function Footer () {
     return(
         <footer className={style.Footer}>
-            <h3>@</h3>
+            <h3>© 2024 Paulo Amancio. Todos os direitos reservados. Inovação para um futuro melhor. Termos de Uso | Política de Privacidade</h3>
 
         </footer>
     )

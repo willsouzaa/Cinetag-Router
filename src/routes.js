b7f7f1b20b34f041fe9acd5fd8742c726077f5d9
Will souza <111616919+willsouzaa@users.noreply.github.com>
2024-09-16 06:32:59 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,21 +4,20 @@ import Favoritos from "./pages/Favoritos";
 import Player from "./pages/Player";
 import NaoEncontrada from './pages/NaoEncontrada';
 import PaginaBase from "./pages/PaginaBase";
+
 function AppRoutes() {
-    return(
-        /*O componente BrowserRouter é responsável por informar a nossa aplicação que teremos um roteamento de componentes a seguir*/
-        /*o Routes é o próprio roteador e o Route é a rota em específico.*/
-        <BrowserRouter >
-        <Routes>
-            <Route path="/" element= {<PaginaBase />}>
-            <Route index element={<Inicio />}></Route>
-            <Route path="favoritos" element={<Favoritos />}></Route>
-            <Route path=":id" element={<Player />}></Route>
-            <Route path="*" element={<NaoEncontrada />}></Route>
-            </Route>
-        </Routes>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<PaginaBase />}>
+                    <Route index element={<Inicio />} />
+                    <Route path="favoritos" element={<Favoritos />} />
+                    <Route path=":id" element={<Player />} />
+                    <Route path="*" element={<NaoEncontrada />} />
+                </Route>
+            </Routes>
         </BrowserRouter>
-    )
+    );
 }
 
 export default AppRoutes;
